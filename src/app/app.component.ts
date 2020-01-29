d2d4b4c5622287d34b45329157d45abec4db27eb
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-29 17:52:21 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,6 @@
 import { Component } from '@angular/core';
 
-function  log(  target,  name,  descriptor){
-  console.log(  target,  name,  descriptor);
 
-  //  store the  original  fun  in   a  var
-
-  const  original =  descriptor.value;
-
-  //  do  some manipulations   with  description .value
-  descriptor.value = function(...args){
-    console.log('arguments', args,'were passed  in this  function');
-    
-    const  result  = original(args)
-
-    console.log('the  result  of the  funtion  is',  result);
-    
-  }
- 
-  //return  the  description
-  return  descriptor;
-
-}
 
 @Component({
   selector: 'app-root',
@@ -30,19 +10,4 @@ function  log(  target,  name,  descriptor){
 export class AppComponent {
   title = 'myapp';
 
-  constructor(){
-    console.log('this statement was  generated  by  the  constructor', this.aSimpleMethod(5));
-    
-   
-
-  }
-
-  @log
-aSimpleMethod(a){
-return a*a  
 }
-
-}
-
-
-
